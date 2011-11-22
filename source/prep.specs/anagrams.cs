434d59043580778fc78ce89c640e987d0f491c15
jonhack <jonathanh@iqmetrix.com>
2011-11-22 09:11:59 -0600

    Pushing new changes

---
+++ source/prep.specs/anagrams.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace prep.specs
+{
+    public class Anagrams
+    {
+        public string Filename = "C:\course\prep\Wordlist.txt";
+        public List<string> Wordlist;
+
+
+        public Anagrams()
+        {
+            FillListFromFile();
+        }
+
+        public void FillListFromFile()
+        {
+            string[] words = System.IO.File.ReadAllLines(Filename);
+            HashLetterCount(words);
+        }
+
+        public void HashLetterCount(string[] words)
+        {
+            foreach (var word in words)
+            {
+                
+            }
+        }
+    }
+}
