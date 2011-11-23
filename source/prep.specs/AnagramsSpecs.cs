7dce569d1031ab0c71c25e2909e7c4d5bd3f5969
jonhack <jonathanh@iqmetrix.com>
2011-11-23 08:53:21 -0600

    Pushing new changes

---
+++ source/prep.specs/AnagramsSpecs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using prep.codekata;
+
+namespace prep.specs
+{
+    [Subject(typeof(Anagrams))]
+    public class AnagramsSpecs
+    {
+        public class when_loading_wordlist
+        {
+            It should_load_80386_words = () =>
+            {
+                //number_of_words.should
+            }
+        }
+    }
+}
---
+++ source/prep/codekata/Anagrams.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace prep.codekata
+{
+    public class Anagrams
+    {
+        private string Filename = "c:\\course\\prep\\words.txt";
+        public string[] WordList;
+
+        public Anagrams()
+        {
+            ReadFileToWordList();
+        }
+
+        public void ReadFileToWordList()
+        {
+            WordList = File.ReadAllLines(Filename);
+        }
+    }
+}
